d52132a3d38dc307e55d38c01e16366db468336a
wiktorrajca <143457758+wiktorrajca@users.noreply.github.com>
2024-04-20 20:00:24 -0700

    HandPoses
    Hit, Split, Surrender, Stand

---
+++ Assets/poses/alert.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alert : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+
+    public void Alert(){
+        Debug.LogError("THUMBS UP");
+    }
+}
